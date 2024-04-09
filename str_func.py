17624c039082452b72a6600620c35c72ca156b6d
Vladimir Blyntsev <blyncev@yandex.ru>
2024-04-09 22:21:42 +0700

    Add to str_func.py function to capitalize first letters in all words

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def capitalize_all_letters(text):
+    """
+    Capitalize all letter in string.
+    """
     return text.upper()
+
+def capitalize_first_letters(text):
+    """
+    Capitalize first letters in all words
+    """
+    return " ".join(word.capitalize() for word in text.split())
